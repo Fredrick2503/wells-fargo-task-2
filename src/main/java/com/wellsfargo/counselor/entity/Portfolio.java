6cefd09dd9b04d2ef469fabed0b09122f632861c
Fredrick George F <fredrick.george.f.25@gmail.com>
2026-07-01 13:55:55 +0000

    entity jpa files

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,46 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false, unique = true)
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Security> securities = new ArrayList<>();
+
+    protected Portfolio() {}
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
